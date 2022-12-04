767168caf97b5eeb1b2c2c35119fdcc18b8b56e5
kenny <linjinjia047@162.com>
2022-12-04 23:23:43 +0800

    icon changes with theme

--- src/main/java/icons/MyIcons.java
+++ src/main/java/icons/MyIcons.java
@@ -10,13 +10,7 @@ import javax.swing.Icon;
  */
 public interface MyIcons {
 
-    Icon DEPLOY_LIGHT = IconLoader.getIcon("/icons/deploy_light.svg", MyIcons.class);
+    Icon DEPLOY = IconLoader.getIcon("/icons/deploy.svg", MyIcons.class);
 
     Icon DEPLOY_DARK = IconLoader.getIcon("/icons/deploy_dark.svg", MyIcons.class);
-
-    Icon DEPLOY_BLUE = IconLoader.getIcon("/icons/deploy_blue.svg", MyIcons.class);
-
-    Icon MESSAGE_BLUE = IconLoader.getIcon("/icons/message_blue.svg", MyIcons.class);
-
-
 }
